5602ebcc5e0c958d6eb19e8a41facbabf7b2ab36
Sangrak Choi <kars@kargn.as>
2017-11-11 18:25:39 +0900

    Fix bugs

--- src/Query/MongoDBBuilder.php
+++ src/Query/MongoDBBuilder.php
@@ -5,4 +5,14 @@ namespace Watson\Rememberable\Query;
 class MongoDBBuilder extends \Jenssegers\Mongodb\Query\Builder
 {
     use BuilderTrait;
+
+    /**
+     * Generate the unique cache key for the query.
+     *
+     * @return string
+     */
+    public function generateCacheKey()
+    {
+        return parent::generateCacheKey();
+    }
 }
